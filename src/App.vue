c66a06fd4f13c659b4b97a6441eb7db062e66164
AyubovAsadbek <ayubovasadbek33@gmail.com>
2025-03-22 14:21:56 +0500

    fix: errors

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,41 @@ import NavbarComponent from "./components/NavbarComponent.vue";
 import FooterComponent from "./components/FooterComponent.vue";
 import CursoreComponent from "./components/CursoreComponent.vue";
 import { defineComponent, computed, onMounted } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import { useAuthStore } from "@/stores/auth";
+import { useRoute } from "vue-router";
+// import { useRoute, useRouter } from "vue-router";
+// import { useAuthStore } from "@/stores/auth";
 import { Icon } from "@iconify/vue";
 
 export default defineComponent({
   components: { Icon, NavbarComponent, FooterComponent, CursoreComponent },
   setup() {
     const route = useRoute();
-    const router = useRouter();
-    const authStore = useAuthStore();
+    // const router = useRouter();
+    // const authStore = useAuthStore();
 
     const hideNavAndFooter = computed(() => {
       return route.path === "/signIn" || route.path === "/signUp";
     });
     onMounted(async () => {
-        const token = '21|jiqhLBDypQkSfdpNto4MGQSK8gpR8Ih3Jbv9bDDh70f46404'
-        localStorage.setItem("auth_token", token as string);
+      const token = "21|jiqhLBDypQkSfdpNto4MGQSK8gpR8Ih3Jbv9bDDh70f46404";
+      localStorage.setItem("auth_token", token as string);
 
-        // if (token) {
-        //   try {
-        //     await authStore.checkAuthentication();
+      // if (token) {
+      //   try {
+      //     await authStore.checkAuthentication();
 
-        //     if (!authStore.isAuthenticated) {
-        //       router.push("/signIn");
-        //     }
-        //   } catch (error) {
-        //     console.error("Error during authentication check:", error);
-        //     router.push("/signIn");
-        //   }
-        // } else {
-        //   if (route.path !== "/signIn" && route.path !== "/signUp") {
-        //     router.push("/signIn");
-        //   }
-        // }
+      //     if (!authStore.isAuthenticated) {
+      //       router.push("/signIn");
+      //     }
+      //   } catch (error) {
+      //     console.error("Error during authentication check:", error);
+      //     router.push("/signIn");
+      //   }
+      // } else {
+      //   if (route.path !== "/signIn" && route.path !== "/signUp") {
+      //     router.push("/signIn");
+      //   }
+      // }
     });
 
     return {
--- src/views/NotFound404View.vue
+++ src/views/NotFound404View.vue
@@ -1,17 +1,16 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
-export default defineComponent({
-
-});
+export default defineComponent({});
 </script>
 
-
 <template>
-    <div class="flex flex-col items-center justify-center gap-10 w-full h-[calc(100vh-110px)] text-center">
-        <div class=" flex flex-col rounded-2xl p-10 bg-white">
-            <h1 class=" text-9xl text-Red">404</h1>
-            <p class=" text-lg text-LightRed">Страница не найдена</p>
-        </div>
+  <div
+    class="flex flex-col items-center justify-center gap-10 w-full h-[calc(100vh-110px)] text-center"
+  >
+    <div class="flex flex-col rounded-2xl p-10 bg-white">
+      <h1 class="text-9xl text-Red">404</h1>
+      <p class="text-lg text-LightRed">Страница не найдена</p>
     </div>
+  </div>
 </template>
